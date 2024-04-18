17799feb6c62b7d05aa282b0f5cbf65f4de65d49
Jiří Krblich <krblichjiri@gmail.com>
2024-04-19 00:22:30 +0200

    new design

--- script/index.js
+++ script/index.js
@@ -1,31 +1,33 @@
 var map = L.map('map').setView([50.0755, 14.4200], 13); // Praha coordinates
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+
+// Použití API zdroje od MapTiler pro dlaždice
+L.tileLayer('https://api.maptiler.com/maps/toner-v2-lite/{z}/{x}/{y}.png?key=f4eoSrX6ObTqEuqprnoI', {
     maxZoom: 19,
-    attribution: '© OpenStreetMap contributors'
+    attribution: '© MapTiler'
 }).addTo(map);
 
 map.zoomControl.setPosition('bottomright');
 
-// Function to add pins from JSON file with custom pin color
+// Funkce pro přidání pinů z JSON souboru s vlastním stylem
 function addPinsFromJSON(jsonUrl) {
     $.getJSON(jsonUrl, function (data) {
         $.each(data, function (key, val) {
-          var pin = $('<div class="custom-pin"></div>');
-          var pinColor = getPinColor(val.color);
-          pin.addClass(pinColor + '-pin');
-          var marker = L.marker([val.lat, val.lon], {icon: L.divIcon({className: 'leaflet-div-icon', html: pin[0]})}).addTo(map);
+            var pin = $('<div class="custom-pin"></div>');
+            var pinColor = getPinColor(val.color);
+            pin.addClass(pinColor + '-pin');
+            var marker = L.marker([val.lat, val.lon], {icon: L.divIcon({className: 'leaflet-div-icon', html: pin[0]})}).addTo(map);
 
-          pin.on('click', function(e){
-              $('#pinAuthor').text("Author: " + val.autor);
-              $('#pinFoudry').text("Foundry: " + val.foundry);
-              $('#pinRelease').text("Release: " + val.release);
-              $('#pinLocation').text("Place: " + val.misto);
-              $('#pinCategory').text("Category: " + val.category);
-              $('#pinImage').attr('src', val.image);
-              $('#fontImage').attr('src', val.fontimage);
-              $('.modal').css('display', 'block');
-          });
-
+            pin.on('click', function(e){
+                // Zobrazení informací o pinu ve vaší modální
+                $('#pinAuthor').text("Author: " + val.autor);
+                $('#pinFoudry').text("Foundry: " + val.foundry);
+                $('#pinRelease').text("Release: " + val.release);
+                $('#pinLocation').text("Place: " + val.misto);
+                $('#pinCategory').text("Category: " + val.category);
+                $('#pinImage').attr('src', val.image);
+                $('#fontImage').attr('src', val.fontimage);
+                $('.modal').css('display', 'block');
+            });
         });
     });
 }
@@ -44,34 +46,34 @@ function getPinColor(colorName) {
     }
 }
 
-// Call the function with the path to JSON file
-addPinsFromJSON('locations/pins.json'); // Change the file name and path as needed
+// Zavolejte funkci s cestou k JSON souboru
+addPinsFromJSON('locations/pins.json'); // Upravte název souboru a cestu podle potřeby
 
-// Close the modal when the close button is clicked
+// Uzavření modálního okna při kliknutí na tlačítko zavřít
 $('.close').click(function() {
     $('.modal').css('display', 'none');
 });
 
 document.addEventListener('keydown', function(event) {
-  if (event.key === 'Escape') {
-    $('.modal').css('display', 'none');
-  }
+    if (event.key === 'Escape') {
+        $('.modal').css('display', 'none');
+    }
 });
 
-// Close the modal when the user clicks outside of it
+// Uzavření modálního okna při kliknutí mimo něj
 $(window).click(function(event) {
     if (event.target == $('.modal')[0]) {
         $('.modal').css('display', 'none');
     }
 });
 
-// Filter pins by color
+// Filtrace pinů podle barvy
 $('.filter-btn').click(function() {
     var color = $(this).data('color');
     if (color === 'all') {
         $('.custom-pin').show();
     } else {
         $('.custom-pin').hide();
-        $('.' + color + '-pin').show(); // Filtrování podle třídy odpovídající barvě pinu
+        $('.' + color + '-pin').show(); // Filtrace podle třídy odpovídající barvě pinu
     }
 });
